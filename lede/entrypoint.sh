a5b58e770293d6e730e67204579cfb34d16e1407
zaber <20830726+ZaberKo@users.noreply.github.com>
2022-06-13 21:23:15 +0800

    fix bugs in lede

--- lede/entrypoint.sh
+++ lede/entrypoint.sh
@@ -4,5 +4,6 @@ set -euxo pipefail
 
 # read -p "please input you username > " USERNAME
 # useradd -m -G sudo ${USERNAME}
+echo "setting password for user ${USERNAME}:"
 passwd ${USERNAME}
-gosu ${USERNAME} /bin/bash
+exec gosu ${USERNAME} "$@"
